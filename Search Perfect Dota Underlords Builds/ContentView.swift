bdaeb016dca1004f6aa97e2628d7a34fcede75ab
Yinchu Xia <larry.fantasy@gmail.com>
2020-03-20 15:31:35 -0700

    Initial Commit

---
+++ Search Perfect Dota Underlords Builds/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Search Perfect Dota Underlords Builds
+//
+//  Created by YINCHU XIA on 3/20/20.
+//  Copyright © 2020 rustgym. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
